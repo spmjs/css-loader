3745b2feb7cdf691c0c4c0a030c887b4c03cad7c
chencheng <sorrycc@gmail.com>
2015-03-31 15:11:35 +0800

    feat: use loaderUtils's urlToRequest

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ module.exports = function(content, map) {
 						this.loaderIndex,
 						this.loaderIndex + 1 + importLoaders
 					).map(function(x) { return x.request; }).join("!") + "!" +
-					urlToRequest(imp.url);
+          loaderUtils.urlToRequest(imp.url);
 				result.push("require(" + JSON.stringify(require.resolve("./mergeImport")) + ")(exports, require(" + JSON.stringify(importUrl) + "), " + JSON.stringify(imp.media.join("")) + ");");
 			}
 		}, this);
@@ -168,44 +168,4 @@ function annotateUrls(tree) {
 			}
 		}
 	}
-}
-
-function urlToRequest(url, root) {
-  var moduleRequestRegex = /^[^?]*~/;
-  var request;
-
-  if(root !== undefined && root !== false && /^\//.test(url)) {
-    // if root is set and the url is root-relative
-    switch(typeof root) {
-      // 1. root is a string: root is prefixed to the url
-      case "string":
-        // special case: `~` roots convert to module request
-        if (moduleRequestRegex.test(root)) {
-          request = root.replace(/([^~\/])$/, "$1/") + url.slice(1);
-        } else {
-          request = root + url;
-        }
-        break;
-      // 2. root is `true`: absolute paths are allowed
-      //    *nix only, windows-style absolute paths are always allowed as they doesn't start with a `/`
-      case "boolean":
-        request = url;
-        break;
-      default:
-        throw new Error("Unexpected parameters to loader-utils 'urlToRequest': url = " + url + ", root = " + root + ".");
-    }
-  } else if(/^\.\.?\//.test(url)) {
-    // A relative url stays
-    request = url;
-  } else {
-    // every other url is threaded like a relative url
-    request = url;
-  }
-
-  // A `~` makes the url an module
-  if (moduleRequestRegex.test(request)) {
-    request = request.replace(moduleRequestRegex, "");
-  }
-
-  return request;
 }
